5d8c8546714a8e6905d8e7dfb0df7756c97c57d0
MAT-AGH <irprgrmr@gmail.com>
2020-11-24 21:25:36 +0330

    removed comment

--- app/src/main/java/ir/mobicar/git/MainActivity.java
+++ app/src/main/java/ir/mobicar/git/MainActivity.java
@@ -12,5 +12,11 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    private void m()
+    {
+
     }
 }
